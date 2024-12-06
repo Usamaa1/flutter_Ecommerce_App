1019dbda6e31b62ac532cce19366584f78e58fb0
usamaa1 <usamariz1997@gmail.com>
2024-12-07 02:26:10 +0500

    Bottom navigation bar with border added and total price section described

--- lib/screens/Cart.dart
+++ lib/screens/Cart.dart
@@ -128,75 +128,85 @@ class _CartState extends State<Cart> {
               ),
             );
           }),
-      bottomNavigationBar: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.25,
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Table(
-                border: const TableBorder(
-                    horizontalInside:
-                        BorderSide(width: 1, color: Colors.black54)),
-                children: const [
-                  TableRow(
-                    children: [
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+            border: Border(
+              top: BorderSide(width: 1, color: Color.fromARGB(52, 182, 181, 181)),
+            ),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            )),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.25,
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Table(
+                  border: const TableBorder(
+                      horizontalInside:
+                          BorderSide(width: 1, color: Colors.black54)),
+                  children: const [
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            "Total Price",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text("5600"),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text("Shipping Fees"),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text("200"),
+                        ),
+                      ],
+                    ),
+                    TableRow(children: [
                       Padding(
                         padding: EdgeInsets.all(8.0),
                         child: Text(
-                          "Total Price",
+                          "Sub Total",
                           style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
+                              fontSize: 18, fontWeight: FontWeight.bold),
                         ),
                       ),
                       Padding(
                         padding: EdgeInsets.all(8.0),
-                        child: Text("5600"),
+                        child: Text(
+                          "5800",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 14),
+                        ),
                       ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("Shipping Fees"),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text("200"),
-                      ),
-                    ],
-                  ),
-                  TableRow(children: [
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        "Sub Total",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        "5800",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 14),
-                      ),
-                    ),
-                  ])
-                ],
-              ),
-              MaterialButton(
-                onPressed: () {},
-                color: Colors.blue,
-                textColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                child: const Text('Checkout'),
-              ),
-            ],
+                    ])
+                  ],
+                ),
+                MaterialButton(
+                  onPressed: () {},
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Text('Checkout'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
