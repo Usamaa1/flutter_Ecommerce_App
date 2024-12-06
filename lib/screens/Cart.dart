d9b9541ec6be2cf1622cdc92244bb4892ac8f9a5
usamaa1 <usamariz1997@gmail.com>
2024-12-07 02:16:29 +0500

    total price is done

--- lib/screens/Cart.dart
+++ lib/screens/Cart.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:full_app/widgets/CartButton.dart';
 import 'package:full_app/widgets/MyAppBar.dart';
+import 'package:full_app/widgets/TextInput.dart';
 
 class Cart extends StatefulWidget {
   const Cart({super.key});
@@ -34,7 +36,7 @@ class _CartState extends State<Cart> {
                     'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80'),
                 title: Text(
                   'Product $index',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
                 subtitle: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -58,26 +60,38 @@ class _CartState extends State<Cart> {
                         GestureDetector(
                           onTap: () {
                             showModalBottomSheet(
+                                isScrollControlled: true,
                                 context: context,
                                 builder: (BuildContext context) {
-                                  return SizedBox(
-                                    height: 300,
-                                    child: Center(
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            "Add Quantity",
-                                            style: TextStyle(fontSize: 22),
-                                          ),
-                                          TextField(
-                                            controller: cont,
-                                          ),
-                                          ElevatedButton(
-                                              onPressed: () {},
-                                              child: Text("Update Quantity"))
-                                        ],
+                                  return Padding(
+                                    padding: EdgeInsets.only(
+                                      bottom: MediaQuery.of(context)
+                                          .viewInsets
+                                          .bottom,
+                                    ),
+                                    child: SizedBox(
+                                      height: 300,
+                                      child: Center(
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            const Text(
+                                              "Add Quantity",
+                                              style: TextStyle(fontSize: 22),
+                                            ),
+                                            TextField(
+                                              controller: cont,
+                                            ),
+                                            const SizedBox(
+                                              height: 20,
+                                            ),
+                                            ElevatedButton(
+                                                onPressed: () {},
+                                                child: const Text(
+                                                    "Update Quantity"))
+                                          ],
+                                        ),
                                       ),
                                     ),
                                   );
@@ -114,6 +128,78 @@ class _CartState extends State<Cart> {
               ),
             );
           }),
+      bottomNavigationBar: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.25,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Table(
+                border: const TableBorder(
+                    horizontalInside:
+                        BorderSide(width: 1, color: Colors.black54)),
+                children: const [
+                  TableRow(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "Total Price",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("5600"),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Shipping Fees"),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("200"),
+                      ),
+                    ],
+                  ),
+                  TableRow(children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Sub Total",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "5800",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 14),
+                      ),
+                    ),
+                  ])
+                ],
+              ),
+              MaterialButton(
+                onPressed: () {},
+                color: Colors.blue,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: const Text('Checkout'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
